f0b2c169a97b2743d7f1b5b451e3ad6f04c8fd17
Fabian205 <fabaguilar205@gmail.com>
2023-03-17 07:51:34 -0500

    Agrego folder Android por icono de app

--- screens/BienvenidosScreen.js
+++ screens/BienvenidosScreen.js
@@ -105,7 +105,7 @@ const styles = StyleSheet.create({
     paddingBottom: 10,
   },
   separator: {
-    marginVertical: 10,
+    marginVertical: 1,
     marginLeft: 30,
     marginRight: 30,
     borderBottomWidth: StyleSheet.hairlineWidth,
